ce918a2748c5c6c7defac8ca81c967ef11ca47ea
ThomasJilge <thomasjilge@gmx.de>
2025-09-30 18:00:17 +0200

    images openstatus added

--- src/components/openStatus.jsx
+++ src/components/openStatus.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import coffeeCup from '../assets/coffeeCup.png';
+import moonIcon from '../assets/moon.png';
+
 
 export default function OpenStatus() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,14 +15,22 @@ export default function OpenStatus() {
 
     const hour = parseInt(bangkokTime);
     const openHour = 7;
-    const closeHour = 17;
+    const closeHour = 18;
 
     setIsOpen(hour >= openHour && hour < closeHour);
   }, []);
 
   return (
-    <div className={`openStatus ${isOpen ? 'open' : 'closed'}`}>
-      {isOpen ? 'open today: 7 a.m. - 6 p.m.' : 'closed: open tomorrow from 7 a.m.'}
-    </div>
+  <div className={`openStatus ${isOpen ? 'open' : 'closed'}`}>
+    <img
+      src={isOpen ? coffeeCup : moonIcon}
+      alt={isOpen ? 'Coffee cup' : 'Moon'}
+      className="statusIcon"
+    />
+    {isOpen
+      ? 'open today: 7 a.m. - 6 p.m.'
+      : 'closed: open tomorrow from 7 a.m.'}
+  </div>
+
   );
 }
