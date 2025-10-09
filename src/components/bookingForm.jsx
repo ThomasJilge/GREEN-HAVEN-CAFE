50552ba954f8ee3ee6c7e604df399ec8a8ceca87
ThomasJilge <thomasjilge@gmx.de>
2025-10-09 17:53:48 +0200

    booking form updated

--- src/components/bookingForm.jsx
+++ src/components/bookingForm.jsx
@@ -1,42 +1,51 @@
 import './bookingForm.css'
 import contactFormImage from '../assets/bkkContactForm.jpg'
+import bookingFormImage from '../assets/bookingFormTwo.jpg'
 
 export default function BookingForm() {
   return (
     <section className='bookingFormContainer' id='bookingForm'>
       <div className="imageContainer">
-        <img className="bookingFormLogo" src={contactFormImage} alt="" />
+        <img className="bookingFormLogo" src={bookingFormImage} alt="" />
         <div className='bookingContainer'>
           <div className='bookingFormHeaderContainer'>
             <h3 className='bookingFormHeadline'>Book your visit</h3>
             <p className='textBookingFormHeadline'>Secure your favorite seat, inside or out.</p>
           </div>
+          <div className='bookingFormMainContainer'>
           <form className='bookingForm' action="">
-            <input type="text" placeholder='Name' />
-            <input type="date" placeholder='Date' />
-            <input type="time" placeholder='Time' />
+            <div className='bookingFormRow'>
+            <div className='bookingFormContainerOne'>
+                <input type="text" placeholder='Name' />
+                <input type="date" placeholder='Date' />
 
-            <select name="people" className="bookingSelect">
-              <option value="">Number of people</option>
-              {[...Array(10)].map((_, i) => (
-                <option key={i + 1} value={i + 1}>{i + 1}</option>
-              ))}
-            </select>
-
-            <div className="seatingOptions">
-              <label className='label'>
-                <input type="radio" name="seating" value="inside" defaultChecked />
-                Inside
-              </label>
-              <label className='label'>
-                <input type="radio" name="seating" value="outside" />
-                Outside
-              </label>
+                <select name="people" className="bookingSelect">
+                  <option value="">Number of people</option>
+                  {[...Array(10)].map((_, i) => (
+                    <option key={i + 1} value={i + 1}>{i + 1}</option>
+                  ))}
+                </select>
             </div>
-
-            <textarea placeholder='Anything we should know?' />
+            <div className='bookingFormContainerTwo'>
+                  <input type="time" placeholder='Time' />
+              <div className="seatingOptions">
+                <label className='label'>
+                  <input type="radio" name="seating" value="inside" />
+                  Inside
+                </label>
+                <label className='label'>
+                  <input type="radio" name="seating" value="outside" />
+                  Outside
+                </label>
+              </div>
+            </div>
+            </div>
+             <div className='messageContainer'>
+                  <textarea placeholder='Anything we should know?' />
+            </div>
             <button className='buttonContactForm'>BOOK NOW</button>
           </form>
+          </div>
         </div>
       </div>
     </section>
