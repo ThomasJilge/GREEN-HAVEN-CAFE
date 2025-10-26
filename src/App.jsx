b481614d3446818be1d205ac77b70edb780e694c
ThomasJilge <thomasjilge@gmx.de>
2025-10-26 16:10:49 +0100

    section location updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Header from './components/header.jsx'
 import FirstSection from './components/firstSection.jsx'
 import AboutUs from './components/aboutUs.jsx'
 import BookingForm from './components/bookingForm.jsx'
-import Location from './components/location.jsx'
 import Menu from './components/menu.jsx'
+import Location from './components/location.jsx'
 import ContactForm from './components/contactForm.jsx'
 import Footer from './components/footer.jsx'
 
@@ -14,8 +14,8 @@ export default function App() {
     <FirstSection />
     <AboutUs />
     <BookingForm />
-    <Location />
     <Menu />
+    <Location />
     <ContactForm />
     <Footer /></>
   )
--- src/components/location.jsx
+++ src/components/location.jsx
@@ -6,9 +6,12 @@ import { useState } from 'react';
 
 export default function Location() {
   return (
-    <section className='aboutUs' id='aboutUs'>
-
-
+    <section className='location' id='location'>
+        <div className='locationHeaderContainer'>
+            <div className='locationHeader'>
+                <h2>Location</h2>
+            </div>
+        </div>
         <div className='googleMapsContainer' id='location'>
             <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29796.737560132588!2d100.46242430034351!3d13.718547596849811!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2985f39205789%3A0x30100b25de24e70!2sThonburi%2C%20Bangkok%2010600%2C%20Thailand!5e1!3m2!1sde!2sde!4v1758363396366!5m2!1sde!2sde"
