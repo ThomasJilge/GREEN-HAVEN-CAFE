d39a5540ecbd8ee45ed5f1f2051201fade044225
ThomasJilge <thomasjilge@gmx.de>
2025-11-07 16:57:12 +0100

    header responsive + burger menu added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,59 @@
-import headerLogo from '../assets/greenHavenCafe.png'
-import './Header.css'
+import { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import headerLogo from '../assets/greenHavenCafe.png';
+import './Header.css';
 import OpenStatus from './openStatus';
-import './openStatus.css'
+import './openStatus.css';
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleMobileMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <section className="header" id='header'>
-        <div className='headerContainer'>
+    <section className="header" id="header">
+      <div className="headerContainer">
+        <div className="headerLogoContainer">
           <a href="">
-            <img className='headerLogo' src={headerLogo} alt="Logo" />
+            <img className="headerLogo" src={headerLogo} alt="Logo" />
           </a>
-          <div className='navBarContainer'>
-        <nav className="navbarHeader">
+        </div>
+
+        <div className="burgerMenu" onClick={toggleMobileMenu}>
+          <FaBars size={28} />
+        </div>
+
+        <div ref={menuRef} className={`navBarContainer ${menuOpen ? 'showMenu' : ''}`}>
+          <nav className="navbarHeader">
             <a href="#aboutUs" className="navbarLink">About Us</a>
             <a href="#menu" className="navbarLink">Menu</a>
             <a href="#location" className="navbarLink">Location</a>
             <a href="#contactForm" className="navbarLink">Contact</a>
-        </nav>
+          </nav>
         </div>
-                <div className='openCloseContainer'>
+        <div className="openCloseContainer">
           <OpenStatus />
         </div>
       </div>
     </section>
-  )
+  );
 }
+
+
